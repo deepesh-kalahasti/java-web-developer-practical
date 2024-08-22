82175ac3d4b8e576fe48e50968b863bd76904b9a
deepesh-kalahasti <deepesh.kalahasti@oracle.com>
2024-08-22 16:51:23 +0530

    Added CRUD operations

--- src/main/java/com/example/demo/entity/Product.java
+++ src/main/java/com/example/demo/entity/Product.java
@@ -12,12 +12,40 @@ public class Product {
  private String description;
  private float price;
 
- protected Product() {
+ public Product() {
  }
 
  @Id
  @GeneratedValue(strategy = GenerationType.IDENTITY)
  public Long getId() {
      return id;
+ }
+ 
+ public void setId(long id) {
+	 this.id = id;
+ }
+ 
+ public String getName() {
+	 return name;
+ }
+ 
+ public void setName(String name) {
+	 this.name = name;
+ }
+ 
+ public String getDescription() {
+	 return description;
+ }
+ 
+ public void setDescription(String description) {
+	 this.description = description;
+ }
+ 
+ public float getPrice() {
+	 return price;
+ }
+ 
+ public void setPrice(float price) {
+	 this.price = price;
  }
 }
