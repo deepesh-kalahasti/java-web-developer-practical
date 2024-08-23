04a34cb4fd176c8ba8094de63ac336216a487079
deepesh-kalahasti <deepesh.kalahasti@oracle.com>
2024-08-23 14:18:47 +0530

    authorisation completed

---
+++ src/main/java/com/example/demo/securty/WebSecurityConfig.java
@@ -0,0 +1,51 @@
+package com.example.demo.securty;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import com.example.demo.service.UserDetailsServiceImpl;
+ 
+@Configuration
+public class WebSecurityConfig {
+ 
+    @Bean
+    UserDetailsService userDetailsService() {
+        return new UserDetailsServiceImpl();
+    }
+     
+    @Bean
+    BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+     
+    @Bean
+    DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+         
+        return authProvider;
+    }
+ 
+    @Bean
+    SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(auth -> auth
+                .requestMatchers("/").hasAnyAuthority("USER", "ADMIN")
+                .requestMatchers("/new").hasAnyAuthority("ADMIN")
+                .requestMatchers("/edit/**").hasAnyAuthority("ADMIN")
+                .requestMatchers("/delete/**").hasAuthority("ADMIN")
+                .anyRequest().authenticated()
+            )
+            .formLogin(login -> login.permitAll())
+            .logout(logout -> logout.permitAll())
+            .exceptionHandling(eh -> eh.accessDeniedPage("/403"))
+            ;
+         
+        return http.build();
+    }
+}
